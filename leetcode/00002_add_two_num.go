768cd53b22e12fe5a40d623574bd975822ea22fc
mystic <215104920@qq.com>
2020-12-28 17:44:33 +0800

    [LeetCode] 00002

--- leetcode/00002_add_two_num.go
+++ leetcode/00002_add_two_num.go
@@ -7,5 +7,38 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-
+	/*
+	# Python Implementation
+	def add_two_numbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+	   dummy = cur = ListNode()
+	   carry = 0
+	   while l1 or l2 or carry:
+		   if l1:
+			   carry += l1.val
+			   l1 = l1.next
+		   if l2:
+			   carry += l2.val
+			   l2 = l2.next
+		   cur.next = ListNode(carry % 10)
+		   cur = cur.next
+		   carry //= 10
+	   return dummy.next
+	*/
+	carry := 0
+	dummy := &ListNode{}
+	cur := dummy
+	for l1 != nil || l2 != nil || carry != 0 {
+		if l1 != nil {
+			carry += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			carry += l2.Val
+			l2 = l2.Next
+		}
+		cur.Next = &ListNode{Val: carry % 10}
+		cur = cur.Next
+		carry /= 10
+	}
+	return dummy.Next
 }
