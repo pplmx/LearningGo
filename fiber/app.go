19584b5435e58be6722f5ef7366ffff93bb27982
Mystic <215104920@qq.com>
2021-06-09 10:22:59 +0800

    feat(fiber): learning fiber in action

--- fiber/app.go
+++ fiber/app.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+    "github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/cors"
+    "log"
+)
 
 func main() {
     app := fiber.New()
 
+    app.Use(cors.New())
+
+    // Or extend your config for customization
+    app.Use(cors.New(cors.Config{
+        AllowOrigins: "https://gofiber.io, https://gofiber.net",
+        AllowHeaders: "Origin, Content-Type, Accept",
+    }))
+
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
+        return c.JSON(fiber.Map{
+            "message": "Hello World 👋!",
+        })
     })
 
-    app.Listen(":3000")
+    log.Fatal(app.Listen(":3000"))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module LearningGo
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.12.0 // indirect
+require github.com/gofiber/fiber/v2 v2.12.0
