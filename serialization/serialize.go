bbb14b8f3f35afd7b3c5fe3afde5261e622e0ddf
Mystic <215104920@qq.com>
2021-10-10 11:43:43 +0800

    feat(serialize): recursively serialize a struct

--- serialization/serialize.go
+++ serialization/serialize.go
@@ -10,59 +10,70 @@ import (
 // https://stackoverflow.com/questions/12854125/how-do-i-dump-the-struct-into-the-byte-array-without-reflection/12854659#12854659
 
 type Profile struct {
-    Id int
-    Name string
+    Id       int
+    Name     string
     password string
 }
 
 type Demo struct {
-    demoId int
-    demoName string
-    userProfile Profile
+    DemoId      int
+    DemoName    string
+    UserProfile Profile
+}
+
+func EncodeRecursive(encoder *gob.Encoder, v reflect.Value) {
+    switch v.Kind() {
+    case reflect.Ptr:
+        EncodeRecursive(encoder, v)
+    case reflect.Interface:
+        EncodeRecursive(encoder, v)
+    case reflect.Struct:
+        EncodeRecursive(encoder, v)
+    case reflect.Slice:
+        EncodeRecursive(encoder, v)
+    case reflect.Map:
+        EncodeRecursive(encoder, v)
+    default:
+        EncodeRecursive(encoder, v)
+    }
 }
 
 func GobEncode(obj interface{}) ([]byte, error) {
     w := new(bytes.Buffer)
     encoder := gob.NewEncoder(w)
-
     v := reflect.ValueOf(obj)
-    fmt.Println(v.NumField())
-    for i := 0; i < v.NumField(); i++ {
-        err := encoder.Encode(v.Field(i).Interface())
-        if err != nil {
-            return nil, err
-        }
-    }
+    EncodeRecursive(encoder, v)
     return w.Bytes(), nil
 }
 
-//func GobDecode(buf []byte, obj *interface{}) (*interface{}, error) {
-//    r := bytes.NewBuffer(buf)
-//    decoder := gob.NewDecoder(r)
-//
-//    v := reflect.ValueOf(obj)
-//    for i := 0; i < v.NumField(); i++ {
-//        err := decoder.Decode(v.Field(i).Interface())
-//        if err != nil {
-//            return nil, err
-//        }
-//    }
-//    return decoder.Decode(&d.name)
-//}
+func GobDecode(buf []byte, obj *interface{}) error {
+    r := bytes.NewBuffer(buf)
+    decoder := gob.NewDecoder(r)
+
+    v := reflect.ValueOf(obj)
+    for i := 0; i < v.NumField(); i++ {
+        err := decoder.Decode(&obj)
+        if err != nil {
+            return err
+        }
+    }
+    return nil
+}
 
 func main() {
     demo := Demo{
-        demoId:      666,
-        demoName:    "Demo Case",
-        userProfile: Profile{
-            Id: 999,
-            Name: "Tom",
+        DemoId:   666,
+        DemoName: "Demo Case",
+        UserProfile: Profile{
+            Id:       999,
+            Name:     "Tom",
             password: "Passw0rd",
         },
     }
-    encode, err := GobEncode(demo)
-    if err != nil {
-        return
-    }
-    fmt.Println(encode)
+    fmt.Printf("%+v\n", demo)
+    //encode, err := GobEncode(demo)
+    //if err != nil {
+    //    return
+    //}
+    //fmt.Println(encode)
 }
