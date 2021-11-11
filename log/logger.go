67f44199e883e9f8f3b3b04d7829d4579ea23564
Mystic <215104920@qq.com>
2021-11-11 10:50:01 +0800

    feat(log): optimize the log logic

--- log/logger.go
+++ log/logger.go
@@ -27,49 +27,6 @@ func init() {
     LOG = createLogger()
 }
 
-func newLogger() *zap.Logger {
-    config := zap.Config{
-        Level:             zap.NewAtomicLevelAt(zap.DebugLevel),
-        Development:       false,
-        DisableCaller:     false,
-        DisableStacktrace: false,
-        Sampling:          nil,
-        Encoding:          "json",
-        EncoderConfig: zapcore.EncoderConfig{
-            MessageKey:       "msg",
-            LevelKey:         "level",
-            TimeKey:          "time",
-            NameKey:          "logger",
-            CallerKey:        "file",
-            FunctionKey:      "func",
-            StacktraceKey:    "stacktrace",
-            LineEnding:       zapcore.DefaultLineEnding,
-            EncodeLevel:      zapcore.LowercaseLevelEncoder,
-            EncodeTime:       zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05.000Z0700"),
-            EncodeDuration:   zapcore.SecondsDurationEncoder,
-            EncodeCaller:     zapcore.ShortCallerEncoder,
-            EncodeName:       zapcore.FullNameEncoder,
-            ConsoleSeparator: "",
-        },
-        OutputPaths:      []string{"stdout", AppLog + "/octopus.log"},
-        ErrorOutputPaths: []string{"stderr", AppLog + "/octopus-error.log"},
-        InitialFields: map[string]interface{}{
-            "app": "octopus",
-        },
-    }
-    logger, err := config.Build()
-    if err != nil {
-        panic(err)
-    }
-    defer func(logger *zap.Logger) {
-        err := logger.Sync()
-        if err != nil {
-            panic(err)
-        }
-    }(logger)
-    return logger
-}
-
 func createLogger() *zap.Logger {
     // First, define our level-handling logic.
     highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
@@ -110,6 +67,8 @@ func createLogger() *zap.Logger {
     development := zap.Development()
     fields := zap.Fields(zap.String("app", "octopus"))
     logger := zap.New(core, caller, development, fields)
+
+    // https://github.com/uber-go/zap/issues/1026
     defer func(logger *zap.Logger) {
         err := logger.Sync()
         if err != nil && !errors.Is(err, syscall.ENOTTY) {
@@ -124,5 +83,5 @@ func getWriteSyncer(path string) zapcore.WriteSyncer {
     if err != nil {
         panic(err)
     }
-    return zapcore.AddSync(file)
+    return file
 }
