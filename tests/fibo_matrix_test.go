45f664c264ae602e5b518a779e32852dab0a0803
Mystic <215104920@qq.com>
2022-09-08 17:12:31 +0800

    test(bench): use table benchmark for matrix fibonacci
    Signed-off-by: Mystic <215104920@qq.com>

--- tests/fibo_matrix_test.go
+++ tests/fibo_matrix_test.go
@@ -6,8 +6,20 @@ import (
 )
 
 func BenchmarkFib(b *testing.B) {
-	n := big.NewInt(200)
-	for i := 0; i < b.N; i++ {
-		fibonacci(n)
+	benchmarks := []struct {
+		name   string
+		fibNum *big.Int
+	}{
+		{name: "Fib_30", fibNum: big.NewInt(30)},
+		{name: "Fib_70", fibNum: big.NewInt(70)},
+		{name: "Fib_200", fibNum: big.NewInt(200)},
+		{name: "Fib_1000", fibNum: big.NewInt(1000)},
+	}
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				fibonacci(bm.fibNum)
+			}
+		})
 	}
 }
