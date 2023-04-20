94e7d0c313e1e7b7e5546ad889ee74b5fa12f090
Mystic <215104920@qq.com>
2023-04-20 10:33:08 +0800

    feat(fiber): Gracefully shutting down

--- fiber/app.go
+++ fiber/app.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/pplmx/LearningGo/fiber/routes"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -11,5 +15,24 @@ func main() {
 	// Handler 404
 	app.Use(routes.HttpNotFound)
 
-	log.Fatal(app.Listen(":3000"))
+	// Listen from a different goroutine
+	go func() {
+		if err := app.Listen(":3000"); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)                    // Create channel to signify a signal being sent
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM) // When an interrupt or termination signal is sent, notify the channel
+
+	_ = <-c // This blocks the main thread until an interrupt is received
+	fmt.Println("Gracefully shutting down...")
+	_ = app.Shutdown()
+
+	fmt.Println("Running cleanup tasks...")
+
+	// Your cleanup tasks go here
+	// db.Close()
+	// redisConn.Close()
+	fmt.Println("Fiber was successful shutdown.")
 }
